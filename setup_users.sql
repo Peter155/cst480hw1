02a66bfd19f25489919358a9fa34622becbb6cc1
Peter155 <110631086+PeterW155@users.noreply.github.com>
2023-02-16 12:12:10 -0500

    Small changes before putting on server

---
+++ setup_users.sql
@@ -0,0 +1,11 @@
+-- password
+INSERT INTO users(username, password) VALUES ('admin', '$argon2id$v=19$m=65536,t=3,p=4$0toyJJQ6Xdv5rUQq1cCoCQ$hYs/2qQrQDy4gld9v4fy0kiQnBzpAu/FWyJgyTq3Ito');
+
+-- abc
+INSERT INTO users(username, password) VALUES ('applesauce', '$argon2id$v=19$m=65536,t=3,p=4$aet/Up/t2f9Bu8teKj5SZA$KTYJ35q136nHVyphnqR3Zs9an5gS0hn1inw5YUoi8TU');
+
+-- fiddlesticks
+INSERT INTO users(username, password) VALUES ('bananabread', '$argon2id$v=19$m=65536,t=3,p=4$KwUDBdwmyFhYtFmdiUI+Nw$aVYp48DsGXYrBYMELdUbj4iO89eS8BwOhK9OPhYHQOE');
+
+-- correcthorsebatterystaple
+INSERT INTO users(username, password) VALUES ('coconutcake', '$argon2id$v=19$m=65536,t=3,p=4$A2+TzjOmpPShDeSaGiBjEg$zf1NFDQYKZWHXhge/f9ZhjrmmnzQ8v8q4UWHXX75SMI');
